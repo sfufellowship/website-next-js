19bf3e69b5c3e4ca99ba970aa4488047610cc32f
Andrew <yy9577@gmail.com>
2025-01-05 21:59:37 -0800

    chore: fix image issue

--- next.config.js
+++ next.config.js
@@ -13,12 +13,15 @@ const nextConfig = {
         dangerouslyAllowSVG: true,
         contentDispositionType: "attachment",
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-        domains: ["localhost"],
         formats: ["image/avif", "image/webp"],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         minimumCacheTTL: 60,
         remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+            },
             {
                 protocol: "https",
                 hostname: "**",
